d3f2df0605cfe2d7af7db3086155a78576854046
Justin <justin.joyce@verizon.net>
2014-10-10 10:59:55 -0400

    finalized the queries, added to python script

---
+++ py/neoTest.py
@@ -0,0 +1,21 @@
+import sys
+from py2neo import neo4j
+
+graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+
+query = neo4j.CypherQuery(graph_db, 'MATCH (patient {infected: "true"})--(room) WHERE patient.infected = "true" return room.name')
+
+rooms = dict()
+
+for record in query.stream() :
+
+	if record[0] in rooms :
+		rooms[record[0]] += 1
+	else :
+		rooms[record[0]] = 1
+
+
+	print(record[0])
+
+for item in rooms.items() :
+	print(item) #+ print(repr(rooms[key]))
